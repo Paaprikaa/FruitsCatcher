e421c9e93ade8f83e0e85a01ace79b547937ae23
Melina Morales <melina.morales@mi.unc.edu.ar>
2025-02-16 22:52:39 -0300

    folder structure + random spawn time

--- Assets/Scripts/FrutaSpawnerScript.cs
+++ Assets/Scripts/FrutaSpawnerScript.cs
@@ -7,27 +7,32 @@ public class FrutaSpawnerScript : MonoBehaviour
     public GameObject[] frutas;
     private float timer = 0;
     private float maxDistance = 8.45f;
+    private float whenToSpawn;
     public LogicScript logic;
 
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        whenToSpawn = Random.Range(1.5f, 3f);
+
     }
 
     void Update()
     {
         if (logic.isAlive)
         {
-            if (timer < 2)
+            if (timer < whenToSpawn)
             {
                 timer += Time.deltaTime;
             }
             else
             {
-                Instantiate(frutas[Random.Range(0, 5)], new Vector3(Random.Range(-maxDistance, maxDistance), transform.position.y, 0), new Quaternion(0, 0 , Random.Range(0,365), 1));
-                // Debug.Log("timer: " + timer);
+                // fruits will spawn at different distances
+                Instantiate(frutas[Random.Range(0, 5)], new Vector3(Random.Range(-maxDistance, maxDistance), transform.position.y, 0), new Quaternion(0, 0, Random.Range(0, 365), 1));
+                // fruits will spawn at random intervals of time
+                whenToSpawn = Random.Range(1.5f, 3f);
+                // timer reset
                 timer = 0;
-                // Debug.Log("timer restart");
             }
         }
     }
